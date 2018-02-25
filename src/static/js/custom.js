843c4b5074cb04a672063be2de3e4af63719d74c
Sanghee Lee <sanghee911@hotmail.com>
2018-02-25 21:40:57 +0900

    add node display

--- src/static/js/custom.js
+++ src/static/js/custom.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
-    const apiUrl = apiServerIP + '/api/v1/namespaces/' + namespace + '/pods/';
+    const apiUrlPod = apiServerIP + '/api/v1/namespaces/' + namespace + '/pods/';
+    const apiUrlNode = apiServerIP + '/api/v1/nodes/';
+    getNodes();
     getPods();
     window.setInterval( function() {
+        getNodes();
         getPods();
     }, 2000);
 
     var app = new Vue({
         el: '#app',
         data: {
-            serverList: []
+            podList: [],
+            nodeList: ['node-1', 'minikube', 'node-3']
         },
         methods: {
-            showServers: function () {
-                // console.log(this.serverList);
-            },
-            updateServers: function (serverList) {
-                this.serverList = serverList;
+            updatePods: function (podList) {
+                this.podList = podList;
             },
             deletePod: function () {
                 const hostname = event.currentTarget.innerHTML;
                 $.ajax({
-                    url: apiUrl + hostname,
+                    url: apiUrlPod + hostname,
                     crossDomain:true,
                     contentType: "application/json",
                     type: 'DELETE',
@@ -28,27 +29,54 @@ $(document).ready(function() {
                         console.log(data)
                     },
                 });
-                // console.log(hostname);
+            },
+            updateNodes: function (nodeList) {
+
             }
         }
     });
 
-    function getPods() {
+    function getNodes() {
         $.ajax({
-            url: apiUrl,
+            url: apiUrlNode,
             crossDomain:true,
             type: 'get',
             success: function(data) {
-                getServers(data);
+                analyseNodeData(data);
             }
         });
     }
 
-    function getServers(data) {
-        var serverList = [];
-        var items = data.items;
+    function analyseNodeData(nodeData) {
+        // console.log(nodeData);
+        var nodeList = [];
+        var items = nodeData.items;
+        for (i = 0; i < items.length; i++) {
+            console.log(items[i].metadata.name);
+            nodeList.push(items[i].metadata.name);
+        }
+
+        // app.nodeList = nodeList;
+    }
+
+    function getPods() {
+        $.ajax({
+            url: apiUrlPod,
+            crossDomain:true,
+            type: 'get',
+            success: function(data) {
+                analysePodData(data);
+            }
+        });
+    }
+
+    function analysePodData(podData) {
+        var podList = [];
+        var items = podData.items;
         var color = 'silver';
         var status;
+        var cpu;
+        var memory;
         for (item of items) {
             if (item.spec.containers[0].env != undefined) {
                 for (var i=0; i < item.spec.containers[0].env.length; i++) {
@@ -66,17 +94,25 @@ $(document).ready(function() {
             } else {
                 status = item.status.phase;
             }
-            serverList.push(
+
+            if (item.spec.containers[0].resources.requests != undefined) {
+               cpu = item.spec.containers[0].resources.requests.cpu;
+               memory = item.spec.containers[0].resources.requests.memory;
+            }
+
+            podList.push(
                 {
                     'hostname': item.metadata.name,
                     'color': color,
                     'status': status,
+                    'cpu': cpu,
+                    'memory': memory,
                     'nodename': item.spec.nodeName
                 }
             );
             // console.log(item.spec.nodeName);
         }
-        app.updateServers(serverList);
+        app.updatePods(podList);
         // console.log('function called');
     }
 });
